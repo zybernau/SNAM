4e7a3871a704edba6b2edf298e8873bf92e5447c
unknown <RXM8076@C9090970E970EF5.amer.homedepot.com>
2016-03-21 17:01:27 -0400

    search

--- client/scripts/controllers/notes.controller.js
+++ client/scripts/controllers/notes.controller.js
@@ -4,24 +4,39 @@ angular
 
 function NotesCtrl($scope, $reactive) {
     $reactive(this).attach($scope);
-    
+
     this.remove = remove;
     this.copy = copy;
+    this.search = search;
+    this.ssg = "";
     this.helpers({
         data() {
-            return Notes.find();
+            // Meteor.call('searchbyTitle', this.getReactively('ssg'), function(data, err) {
+            //     return data;
+            // });
+            // return data1; 
+            if (this.getReactively('ssg') == "")
+                return Notes.find();
+            else
+                return Notes.find({ title: new RegExp('^.*' + this.getReactively('ssg') + '.*$', "i") });
         }
     });
     /// methods
-    function remove(note) {
+    function remove(note) { 
         //this.data.remove(note);
-         Meteor.call('deleteNote', note._id, savedSuccess);
+        Meteor.call('deleteNote', note._id, savedSuccess);
     }
-    
+
     function copy(note) {
         // write logic for copying to clipboard.
     }
     function savedSuccess(e) {
         console.log(e);
+    }
+
+    function search() {
+        if (this.ssg.toString().length > 1) {
+            // Meteor.call('searchbyTitle', this.ssg);    
+        }
     }
 }
